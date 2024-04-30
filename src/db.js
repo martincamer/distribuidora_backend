7239a0c966b92486104ceaea866794d15848f3d6
martincamer <martincamer011@gmail.com>
2024-04-29 23:45:39 -0300

    asd

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 export async function connectDB() {
   try {
-    const connection = await connect(process.env.BACKED_URL);
+    const connection = await connect(`${process.env.BACKED_URL}`);
     console.log("Conexión a la base de datos establecida");
     return connection.connection.db;
   } catch (error) {
