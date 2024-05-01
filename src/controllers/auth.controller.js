b982d96f43f7945b325fad36b7bd5640610b3853
martincamer <martincamer011@gmail.com>
2024-04-30 21:08:59 -0300

    sad

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -72,7 +72,6 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      // httpOnly: process.env.NODE_ENV !== "development",
       httpOnly: false,
       secure: true,
       sameSite: "none",
@@ -88,23 +87,23 @@ export const login = async (req, res) => {
   }
 };
 
-export const verifyToken = async (req, res) => {
-  const { token } = req.cookies;
-  if (!token) return res.send(false);
+// export const verifyToken = async (req, res) => {
+//   const { token } = req.cookies;
+//   if (!token) return res.send(false);
 
-  jwt.verify(token, TOKEN_SECRET, async (error, user) => {
-    if (error) return res.sendStatus(401);
+//   jwt.verify(token, TOKEN_SECRET, async (error, user) => {
+//     if (error) return res.sendStatus(401);
 
-    const userFound = await User.findById(user.id);
-    if (!userFound) return res.sendStatus(401);
+//     const userFound = await User.findById(user.id);
+//     if (!userFound) return res.sendStatus(401);
 
-    return res.json({
-      id: userFound._id,
-      username: userFound.username,
-      email: userFound.email,
-    });
-  });
-};
+//     return res.json({
+//       id: userFound._id,
+//       username: userFound.username,
+//       email: userFound.email,
+//     });
+//   });
+// };
 
 export const logout = async (req, res) => {
   res.cookie("token", "", {
