45a381d614f1a5b99a71d700d9fa4aae69a1ea4c
martincamer <martincamer011@gmail.com>
2024-04-30 21:25:36 -0300

    sad

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import {
   login,
   logout,
   register,
-  // verifyToken,
+  verifyToken,
 } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
@@ -12,8 +12,7 @@ const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
-// router.get("/verify", verifyToken);
-// router.post("/logout", verifyToken, logout);
-router.post("/logout", logout);
+router.get("/verify", verifyToken);
+router.post("/logout", verifyToken, logout);
 
 export default router;
