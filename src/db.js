3e9f7ec26be08392b890362b08412931a61aa4a5
martincamer <martincamer011@gmail.com>
2024-05-11 00:25:23 -0300

    asd

--- src/db.js
+++ src/db.js
@@ -16,15 +16,16 @@
 // }
 
 import mongoose from "mongoose";
+import { MONGO_URL } from "./config";
 
 export async function connectDB() {
-  // if (!process.env.MONGO_URL) {
-  //   throw new Error("Please add the MONGO_URL environment variable");
-  // }
-
-  if (!"mongodb://127.0.0.1:27017/sistema_de_gestion") {
+  if (!MONGO_URL) {
     throw new Error("Please add the MONGO_URL environment variable");
   }
+
+  // if (!"mongodb://127.0.0.1:27017/sistema_de_gestion") {
+  //   throw new Error("Please add the MONGO_URL environment variable");
+  // }
 
   // if (
   //   !"mongodb://mongo:ewvsSdQRijYrJpFkYnCIbviEobUslioo@monorail.proxy.rlwy.net:59925"
@@ -32,11 +33,11 @@ export async function connectDB() {
   //   throw new Error("Please add the MONGO_URL environment variable");
   // }
 
-  // mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect(MONGO_URL);
   // mongoose.connect(
   //   "mongodb://mongo:ewvsSdQRijYrJpFkYnCIbviEobUslioo@monorail.proxy.rlwy.net:59925"
   // );
-  mongoose.connect("mongodb://127.0.0.1:27017/sistema_de_gestion");
+  // mongoose.connect("mongodb://127.0.0.1:27017/sistema_de_gestion");
 
   const database = mongoose.connection;
 
