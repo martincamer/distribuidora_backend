b46848fa2bea6335afdec1c42d63066c5f94f99c
martincamer <martincamer011@gmail.com>
2024-05-15 00:50:00 -0300

    sad2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -84,7 +84,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       // httpOnly: true,
       // secure: true,
-      // sameSite: "none",
+      sameSite: "none",
     });
 
     res.json({
